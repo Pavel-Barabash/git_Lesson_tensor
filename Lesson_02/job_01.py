1700e54020b148631813a023803b6baffe9992ea
Pavel-Barabash <jogris7500@gmail.com>
2022-12-05 19:47:22 +0300

    Homework for lesson 2

---
+++ Lesson_02/job_01.py
@@ -0,0 +1,28 @@
+# Задание 1: Пользователь вводит два числа (возможно дробные), вывести результат их сложения, вычитания, умножения, деления, возведения в степень, деления по модулю, целочисленного деления.
+
+from decimal import Decimal
+
+
+def start(dig):
+    if dig.replace('.', '').isdigit() == True:
+        num = Decimal(dig)
+        return (num)
+    else:
+        print(dig, 'не является числом')
+        print('')
+        return (start(input('Введите повторно число: ')))
+
+
+a = start(input('Введите первое число: '))
+print('')
+b = start(input('Введите второе число: '))
+
+
+print('')
+print(a, '+', b, '=', a + b)
+print(a, '-', b, '=', a - b)
+print(a, '*', b, '=', a * b)
+print(a, '/', b, '=', a / b)
+print(a, '**', b, '=', a**b)
+print(a, '%', b, '=', a % b)
+print(a, '//', b, '=', a // b)
